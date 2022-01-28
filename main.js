2b19c2035da702e8286680a7cf7b8eacc1042f0e
Erick Esqueda <eriesqu19@gmail.com>
2022-01-28 15:33:47 -0800

    added styling, index, and rules

---
+++ main.js
@@ -0,0 +1,4 @@
+const updateRecent = document.querySelector('.update-recent')
+const updatePrevious = document.querySelector('.update-previous')
+const updateOld = document.querySelector('.update-old')
+const updateRecent = document.querySelector('.update-recent')
